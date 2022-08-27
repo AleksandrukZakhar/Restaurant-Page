570481b1ee98747924c5a7b94457e0d0a4d9023b
AleksandrukZakhar <zakharaleksandrukdev@gmail.com>
2022-08-27 20:42:41 +0300

    Add Contact page in js and transfer through pages

--- src/index.js
+++ src/index.js
@@ -7,34 +7,6 @@ import pizza4 from "./assets/pizza-4.jpg";
 import pizza5 from "./assets/pizza-5.jpg";
 import pizza6 from "./assets/pizza-6.jpg";
 
-const Header = () => {
-  const header = document.createElement("header");
-
-  const p = document.createElement("p");
-  p.textContent = "Restaurant";
-  p.classList.add("logo");
-  header.appendChild(p);
-
-  const div = document.createElement("div");
-  div.classList.add("link-container");
-  header.appendChild(div);
-
-  const links = ["Home", "Menu", "Contact Us"];
-
-  for (let i = 0; i < 3; i++) {
-    const pl = document.createElement("p");
-
-    const a = document.createElement("a");
-    a.textContent = links[i];
-
-    pl.appendChild(a);
-
-    div.appendChild(pl);
-  }
-
-  return header;
-};
-
 const Home = () => {
   const main = document.createElement("main");
 
@@ -63,6 +35,8 @@ const Home = () => {
   return main;
 };
 
+let page = Home();
+
 const Menu = () => {
   const main = document.createElement("main");
 
@@ -70,7 +44,7 @@ const Menu = () => {
   div.classList.add("pizza-container");
   main.appendChild(div);
 
-  const pizzas = [pizza1, pizza2, pizza3, pizza4, pizza5, pizza6];
+  const pizzas = ["", pizza1, pizza2, pizza3, pizza4, pizza5, pizza6];
 
   for (let i = 1; i < 7; i++) {
     const img = new Image();
@@ -82,5 +56,78 @@ const Menu = () => {
   return main;
 };
 
+const Contact = () => {
+  const form = document.createElement("form");
+
+  const div = document.createElement("div");
+  form.appendChild(div);
+
+  const inputData = [
+    { placeholder: "Name", title: "Example: John" },
+    { placeholder: "Surname", title: "Example: Doe" },
+  ];
+
+  for (let i = 0; i < 2; i++) {
+    const input = document.createElement("input");
+    input.type = "text";
+    input.placeholder = inputData[i].placeholder;
+    input.pattern = "/[a-z]/gi";
+    input.title = inputData[i].title;
+    div.appendChild(input);
+  }
+
+  const emailInput = document.createElement("input");
+  emailInput.type = "text";
+  emailInput.placeholder = "Email";
+  div.appendChild(emailInput);
+
+  const button = document.createElement("button");
+  button.textContent = "Send";
+  div.appendChild(button);
+
+  return form;
+};
+
+const Header = () => {
+  const header = document.createElement("header");
+
+  const p = document.createElement("p");
+  p.textContent = "Restaurant";
+  p.classList.add("logo");
+  header.appendChild(p);
+
+  const div = document.createElement("div");
+  div.classList.add("link-container");
+  header.appendChild(div);
+
+  const links = ["Home", "Menu", "Contact Us"];
+  const pages = [Home(), Menu(), Contact()];
+
+  for (let i = 0; i < 3; i++) {
+    const pl = document.createElement("p");
+
+    const a = document.createElement("a");
+    a.textContent = links[i];
+
+    a.addEventListener("click", () => {
+      const main = document.querySelector("main");
+      const form = document.querySelector("form");
+
+      if (form === null) {
+        document.body.removeChild(main);
+      } else document.body.removeChild(form);
+
+      page = pages[i];
+      document.body.appendChild(page);
+    });
+
+    pl.appendChild(a);
+
+    div.appendChild(pl);
+  }
+
+  return header;
+};
+
 document.body.appendChild(Header());
-document.body.appendChild(Menu());
+document.body.appendChild(page);
